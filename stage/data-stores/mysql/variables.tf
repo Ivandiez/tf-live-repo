86adeec507954dffe696033a5b97f8aa6abc944d
Ivan Tsyplenkov <vankas2010@yandex.ru>
2021-04-29 16:36:33 +0300

    Initial commit of live repo

---
+++ stage/data-stores/mysql/variables.tf
@@ -0,0 +1,10 @@
+variable "db_password" {
+  description = "The password for the database"
+  type = string
+}
+
+var "db_name" {
+  description = "The Name of the database"
+  type i      = string
+  default     = "example_database_stage"
+}
